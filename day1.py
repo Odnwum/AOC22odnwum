de7c5bfd0f02d6ec170099ae5068e7aacbff2ba7
Odnwum <117602926+Odnwum@users.noreply.github.com>
2022-12-02 20:22:04 +0200

    Update day1.py

--- day1.py
+++ day1.py
@@ -1,16 +1,10 @@
-def readfile(filename):
+def part1(filename):
     with open(filename, "r") as f:
         list = f.read()
     list = list.split("\n")
-    return list  # actually returns array XD
-
-
-def main():
-    filename = "day1.txt"
-    array = readfile(filename)
     arrayofsum = [0]
     j = 0
-    for i in array:
+    for i in list:
         if i != "":
             arrayofsum[j] += int(i)
         else:
@@ -23,6 +17,31 @@ def main():
             elfpos = i
             break
     print(f"Elf number {elfpos} has {richelf} calories ")
+    return arrayofsum
+
+
+def sort(array):
+    if array == []:
+        return []
+    else:
+        pivot = array[0]
+        lesser = sort([x for x in array[1:] if x < pivot])
+        greater = sort([x for x in array[1:] if x >= pivot])
+        return lesser + [pivot] + greater
+
+
+def part2(array):
+    array = sort(array)
+    print(array)
+    top3elvescalories = array[-1] + array[-2] + array[-3]
+    return top3elvescalories
+
+
+def main():
+    filename = "day1.txt"
+    arrayofsum = part1(filename)
+    top3 = part2(arrayofsum)
+    print(top3)
 
 
 if __name__ == "__main__":
